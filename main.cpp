c738bbc0f766704d2f5085a907f2b5ab9bc6b56e
MJV-git <juncarmathias@gmail.com>
2024-12-02 19:31:55 -0500

    Git

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@ int main() {
 
     cout << "Welcome to COP2334 Shipping" << endl << endl;
     cout << "Please Select a Shipping Option:" << endl << endl;
-    cout << "1. Standard Shipping ($0.90 per Oz)" << endl << endl;
-    cout << "2. 2 Day Delivery Shipping ($1.50 per Oz)" << endl << endl;
-    cout << "3. Overnight Shipping (3.00 per Oz)" << endl << endl;
+    cout << "1. Standard Shipping ($0.90 per Oz)" << endl;
+    cout << "2. 2 Day Delivery Shipping ($1.50 per Oz)" << endl;
+    cout << "3. Overnight Shipping (3.00 per Oz)" << endl;
     cin >> shippingType;
     switch (shippingType) {
-        case 1: new Package(); break;
-        case 2: new TwoDayPackage; break;
-        case 3: new OvernightPackage; break;
+        case 1: Package *package1 = new Package(); break;
+        case 2: TwoDayPackage *package2 = new TwoDayPackage(); break;
+        case 3: OvernightPackage *package3 = new OvernightPackage(); break;
     }
     cout << "Please enter the shipping information:" << endl << endl;
     cout << "Enter Recipient Name:" << endl;
